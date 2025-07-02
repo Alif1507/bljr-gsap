6b8c555209583d7106b025ff9c9e2b90c2704c5e
Alif1507 <m.alifwahyudi2007@gmail.com>
2025-07-02 17:07:41 +0700

    pertama akali

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import gsap from "gsap"
+import { ScrollTrigger, SplitText } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger, SplitText) 
+
+const App = () => {
+  return (
+    <div className="text-9xl flex-center">
+      app
+    </div>
+  )
+}
+
+export default App
